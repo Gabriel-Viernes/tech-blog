af4ef7dee6041d66ea4e88da746b67f44328c1c4
Gabriel Viernes <viernesgabriel@gmail.com>
2023-11-29 22:44:29 -0600

    added sessions

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express =  require('express')
 const exphbs = require('express-handlebars')
 const hbs = exphbs.create({})
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const app = express()
+
+const sess = {
+	secret: 'iusearchbtw',
+	cookie: {
+		maxAge: 300000,
+		httpOnly: true,
+		secure: false,
+		sameSite: strict
+	},
+	resave: false,
+	saveUninitialized: false,
+	store: new SequelizeStore({
+		db: sequelize,
+	})
+}
+app.use(session(sess))
 
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 app.set('views', './views')
-
+app.use(express.static('public'))
 app.get('/', (req, res) => {
 	res.render('home')
 })
